4f7d88e9ace09851e5c32b48ad4f2b9d6ab874b5
Guilherme-Galli77 <79452612+Guilherme-Galli77@users.noreply.github.com>
2021-03-15 08:50:34 -0300

    Alteração no codigo

---
+++ Aula05-LAB-Projeto/src/galli/guilherme/Sistema.java
@@ -0,0 +1,49 @@
+package galli.guilherme;
+
+
+import java.util.Scanner;
+
+public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+    private Conta conta;
+
+    public void executar(){
+        int opcao;
+        while (executarSistema) {
+            exibirMenu();
+            opcao = scanner.nextInt();
+            avaliarOpcao(opcao);
+        }
+    }
+
+    private void avaliarOpcao(int opcao) {
+        switch (opcao){
+            case 0:
+                System.out.println("Obrigado por escolher o MauaBank!");
+                this.executarSistema = false;
+                break;
+            case 1:
+                System.out.println("Saldo Atual: R$" + this.conta.getSaldo());
+                break;
+            default:
+                System.out.println("Essa funcionalidade ainda nao foi implementada!");
+        }
+    }
+
+    public Sistema() {
+        this.scanner = new Scanner(System.in); //Le da entrada do sistema
+        this.executarSistema = true;
+        this.conta = new Conta("Mario",1945, 1234);
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Sacar Dinheiro");
+        System.out.println("3 - Depositar Dinheiro");
+        System.out.println("4 - Transferir Dinheiro");
+        System.out.println("5 - Pagar Conta (Titulo)");
+        System.out.println("0 - Encerrar");
+    }
+}
