ef9fb40443e511eb69fe749fef4a88be50ebf980
Guilherme-Galli77 <79452612+Guilherme-Galli77@users.noreply.github.com>
2021-04-29 09:24:59 -0300

    Codigo feito em aula

---
+++ estudo_excecao/src/br/maua/estudo_excecao/testExcecaoString.java
@@ -0,0 +1,9 @@
+public class testExcecaoString {
+    public static void main(String[] args) {
+        String frase = null;
+        String novaFrase = null;
+        novaFrase = frase.toUpperCase();
+        System.out.println("Frase Antiga:" + frase);
+        System.out.println("Frase Nova:" + novaFrase);
+    }
+}
